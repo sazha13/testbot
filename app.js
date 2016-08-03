b3d981ad4ed08806a4c8eda90192da1ff1f98fe0
sasa <sasa@sasa-desktop>
2016-08-03 15:25:35 +0400

    few change

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var mongoose = require('mongoose');
 
 //constants data
 
 var port = process.env.PORT || 3011;
 var msAppId = process.env.MICROSOFT_APP_ID;
 var msAppPassword = process.env.MICROSOFT_APP_PASSWORD;
-//console.log(process.env.MICROSOFT_APP_ID);
-console.log(process.env);
 
 //Setup Restify Server
 var server = restify.createServer();
@@ -15,28 +14,54 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
-server.get('/', respond);
-server.post('/request', handleRequestMessage);
+
  
 // Create chat bot
 var connector = new builder.ChatConnector({appId: msAppId, appPassword: msAppPassword});
-console.log(connector);
 var bot = new builder.UniversalBot(connector);
 
 server.post('/api/messages', connector.listen()); 
 
-bot.dialog('/', function (session) {
-  session.send('Provider bot in operation :-)');
-//	session.send();
-});
+bot.dialog('/', botDialog);
 
+//REST API
+server.get('/', respond);
+server.post('/request', handleRequestMessage);
 
-function respond(req, res, next) {
+//server.use(restify.acceptParser(server.acceptable));
+//server.use(restify.bodyParser());
+//server.use(restify.authorizationParser());
+//
+//server.get('/thread',getThreads);
+//server.get('/thread/:THREAD_ID/messages',getThreadMsgs);
+//server.post('/thread/:THREAD_ID/messages',postThreadMsgs);
+//server.post('/apns',postAPNs);
+//server.post('/createProvider',postCreateProvider);
+//server.post('/thread/:THREAD_ID/message_seen/:MSG_ID',postThreadMsgSeen);
+
+//REST API functions
+function respond(req, res, next) 
+{
 	  res.contentType = "text/plain";
 	  res.send("HERE");
 	  next();
-	}
-function handleRequestMessage(req, res, next) {
+}
+function handleRequestMessage(req, res, next) 
+{
 	  res.send('POST API Response!!!');
 	  next();
-	}
+}
+//bot Functions
+function botDialog(session)
+{
+	session.send('Provider bot in operation :-)');
+	console.log(session);
+}
+//mongoose
+mongoose.connect("mongodb://test:test@ds139685.mlab.com:39685/providerbotv3");
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function() 
+{
+  console.log("connection DB ok");
+});
